81aeb91993a37c8e2c491b2041955f3763e8e04d
Arthur Medeiros <rthurmedeiros@gmail.com>
2022-04-08 23:29:39 -0300

    Implemented game over when planet is destroyed

--- src/game/GameStateUI.gd
+++ src/game/GameStateUI.gd
@@ -2,7 +2,9 @@ extends CanvasLayer
 
 
 onready var animation = $AnimationPlayer
+onready var subtitle_label = $GameOver/Message/Subtitle
 
 
-func call_game_over():
+func call_game_over(subtitle = ''):
 	animation.play("game_over")
+	subtitle_label.text = subtitle
--- src/game/logic/WhenDeadGameOver.gd
+++ src/game/logic/WhenDeadGameOver.gd
@@ -1,16 +1,18 @@
 extends Node
 
 
-export var ship_path: NodePath
+export var target_node_path: NodePath
 export var state_ui_path: NodePath
+export var message = "you died!"
 
-onready var ship = get_node(ship_path)
+onready var target_node = get_node(target_node_path)
 onready var state_ui = get_node(state_ui_path)
 
 
 func _ready():
-	ship.connect("died", self, "_on_Ship_died")
+	var trigger = "died" if target_node.has_signal("died") else "tree_exited"
+	target_node.connect(trigger, self, "_on_TargetNode_exited")
 
 
-func _on_Ship_died():
-	state_ui.call_game_over()
+func _on_TargetNode_exited():
+	state_ui.call_game_over(message)
