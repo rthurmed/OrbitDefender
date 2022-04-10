1dcd8984c5b1f87109fd615e4e3673cb8685a27d
Arthur Medeiros <rthurmedeiros@gmail.com>
2022-04-10 14:35:13 -0300

    Adjusted to stop stage manager on game over

--- src/game/logic/WhenDeadGameOver.gd
+++ src/game/logic/WhenDeadGameOver.gd
@@ -3,10 +3,12 @@ extends Node
 
 export var target_node_path: NodePath
 export var state_ui_path: NodePath
+export var stage_manager_path: NodePath
 export var message = "you died!"
 
 onready var target_node = get_node(target_node_path)
 onready var state_ui = get_node(state_ui_path)
+onready var stage_manager =get_node(stage_manager_path)
 
 
 func _ready():
@@ -16,3 +18,4 @@ func _ready():
 
 func _on_TargetNode_exited():
 	state_ui.call_game_over(message)
+	stage_manager.stop()
