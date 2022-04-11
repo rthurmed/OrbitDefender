c917879ec7d33acb5685bd709cb32e3da307822e
Arthur Medeiros <rthurmedeiros@gmail.com>
2022-04-10 21:33:16 -0300

    Removed buttons and transparency from pause menu

--- src/system/Pause.gd
+++ src/system/Pause.gd
@@ -2,26 +2,20 @@ extends CanvasLayer
 
 
 onready var ui = $UI
-onready var btn_continue = $UI/VBoxContainer/Continue
 
 
-func _input(event):
+func _unhandled_input(event):
 	if event.is_action_released("pause"):
 		paused(not get_tree().paused)
+	
+	if (
+		ui.visible and
+		event.is_action_released("reset")
+	):
+		var _ok = get_tree().reload_current_scene()
+		paused(false)
 
 
 func paused(state: bool):
 	get_tree().paused = state
 	ui.visible = state
-	
-	if ui.visible:
-		btn_continue.grab_focus()
-
-
-func _on_Continue_pressed():
-	paused(false)
-
-
-func _on_Restart_pressed():
-	paused(false)
-	var _ok = get_tree().reload_current_scene()
